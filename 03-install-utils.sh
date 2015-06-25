aa9d5cb026fb7e89a54de9215b76fdf3b404eb9d
Bruno Luiz <contato@brunoluiz.net>
2015-06-25 13:49:40 -0300

    Added execution permits. and added recent can-utils

--- 03-install-utils.sh
+++ 03-install-utils.sh
@@ -10,31 +10,38 @@ cd tmp
 
 # Install LibSocketCAN ############
 
-pkgname=libsocketcan-0.0.10
-if [ ! -d $pkgname ]; then
-then
-  echo "$pkgname folder does not exist... Downloading!"
-  wget http://www.pengutronix.de/software/libsocketcan/download/$pkgname.tar.bz2
-  tar xvjf $pkgname.tar.bz2
-fi
+# pkgname=libsocketcan-0.0.10
+# if [ ! -d $pkgname ]; then
+# then
+#   echo "$pkgname folder does not exist... Downloading!"
+#   wget http://www.pengutronix.de/software/libsocketcan/download/$pkgname.tar.bz2
+#   tar xvjf $pkgname.tar.bz2
+# fi
 
-cd $pkgname
-./autogen.sh
-./configure --prefix=/usr
-make
-make install
+# cd $pkgname
+# ./autogen.sh
+# ./configure --prefix=/usr
+# make
+# make install
 
-# Install CAN-Utils ##############
+# # Install CAN-Utils ##############
 
-pkgname=canutils-4.0.6
-if [ ! -d $pkgname ]; then
-then
-  echo "$pkgname folder does not exist... Downloading!"
-  wget http://pengutronix.de/software/socket-can/download/canutils/v4.0/$pkgname.tar.bz2
-  tar xvjf $pkgname.tar.bz2
-fi
+# pkgname=canutils-4.0.6
+# if [ ! -d $pkgname ]; then
+# then
+#   echo "$pkgname folder does not exist... Downloading!"
+#   wget http://pengutronix.de/software/socket-can/download/canutils/v4.0/$pkgname.tar.bz2
+#   tar xvjf $pkgname.tar.bz2
+# fi
 
-cd $pkgname
+# cd $pkgname
+# ./autogen.sh
+# ./configure --prefix=/usr
+# make
+# make install
+
+git clone https://github.com/linux-can/can-utils
+cd can-utils
 ./autogen.sh
 ./configure --prefix=/usr
 make
